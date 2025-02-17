3198a3691a56f69e86a89b759ca717eced02f45d
franckyy <franck.calestroupat@protonmail.com>
2025-02-17 23:37:31 +0100

    premier commit

---
+++ src/main/java/com/deeplearning/LinearRegression_2neurones/LinearRegression_2neurones.java
@@ -0,0 +1,10 @@
+package com.deeplearning.LinearRegression_2neurones;
+
+public class LinearRegression_2neurones {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
