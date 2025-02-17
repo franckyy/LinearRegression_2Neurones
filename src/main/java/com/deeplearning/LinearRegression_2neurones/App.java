a516519862c172b0ff86e5af85d51871103afe49
franckyy <franck.calestroupat@protonmail.com>
2025-02-17 23:33:50 +0100

    commit initial

---
+++ src/main/java/com/deeplearning/LinearRegression_2neurones/App.java
@@ -0,0 +1,7 @@
+package com.deeplearning.LinearRegression_2neurones;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/com/deeplearning/LinearRegression_2neurones/AppTest.java
@@ -0,0 +1,13 @@
+package com.deeplearning.LinearRegression_2neurones;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
